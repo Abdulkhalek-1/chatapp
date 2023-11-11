1ef8814bc6213f1a5e35017f9cdf5f3423a6e9a6
High-2005 <highgamer2005@gmail.com>
2023-11-11 13:31:36 +0200

    test

--- Back-end/project/project/settings.py
+++ Back-end/project/project/settings.py
@@ -212,7 +212,11 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
 ]
 
-CSRF_TRUSTED_ORIGINS = (["http://localhost:8000"],)
+CSRF_TRUSTED_ORIGINS = (
+    ["http://localhost:8000"],
+    ["http://localhost:5173"],
+)
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:8000",
+    "http://localhost:5173",
 ]
